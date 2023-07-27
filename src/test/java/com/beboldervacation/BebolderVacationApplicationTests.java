20e2e2b2ba600b9d4fab814b6975e51323fcd8a4
Andrés Ibarra Muñoz <andres.ibarra@bebolder.local>
2023-07-27 14:12:03 -0500

    Creación de proyecto y estructura de carpetas

---
+++ src/test/java/com/beboldervacation/BebolderVacationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beboldervacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BebolderVacationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
